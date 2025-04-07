717cde86d55bbf007c512ab432c33f1d42b996b0
Pechukka <eliaspm05@gmail.com>
2025-04-07 09:09:36 +0200

    First commit

---
+++ src/concesionario/Coche.java
@@ -0,0 +1,376 @@
+package concesionario;
+
+public class Coche {
+
+    // Atributos
+    private int id;
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String color;
+    private double precio;
+    private int anio;
+    private boolean vendido;
+
+    private static Coche[] coches = new Coche[100];
+    private static int numCoches = 0;
+    private static int contadorId = 1;
+    
+    // Añadimos 5 coches al comenzar el programa
+    static {
+        coches[0] = new Coche("ABC123", "Toyota", "Corolla", "Rojo", 20000.0, 2021, false);
+        contadorId++;
+        coches[1] = new Coche("XYZ456", "Honda", "Civic", "Azul", 18000.0, 2020, false);
+        contadorId++;
+        coches[2] = new Coche("DEF789", "Ford", "Focus", "Negro", 22000.0, 2022, false);
+        contadorId++;
+        coches[3] = new Coche("GHI012", "Chevrolet", "Malibu", "Blanco", 25000.0, 2021, false);
+        contadorId++;
+        coches[4] = new Coche("JKL345", "Nissan", "Altima", "Gris", 23000.0, 2022, true);
+        contadorId++;
+
+        numCoches = 5;
+    }
+
+    // Constructor
+    public Coche(String matricula, String marca, String modelo, String color, double precio, int anio,
+            boolean vendido) {
+        this.id = contadorId;
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.precio = precio;
+        this.anio = anio;
+        this.vendido = vendido;
+    }
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public boolean getVendido() {
+        return vendido;
+    }
+
+    public void setVendido(boolean vendido) {
+        this.vendido = vendido;
+    }
+
+    // MÉTODOS
+
+    // Método para agregar coches
+    public static String agregarCoche(Coche coche) {
+        if (numCoches >= coches.length) {
+            return "\nNo se pueden agregar más coches";
+        }
+
+        coches[numCoches] = coche;
+        numCoches++;
+        contadorId++;
+        return "\nCoche agregado con ID " + coche.getId();
+    }
+
+    // Método para mostrar los coches existentes en el array
+    public static void mostrarCoches() {
+        if (numCoches == 0) {
+            System.out.println("\nNo hay coches en el concesionario.");
+            return;
+        }
+
+        for (int i = 0; i < numCoches; i++) {
+            Coche coche = coches[i];
+
+            if (coche.getVendido() == true) {
+                System.out.println(
+                        "ID: " + coche.getId() + ", Matrícula: " + coche.getMatricula() + ", Marca: " + coche.getMarca()
+                                + ", Modelo: " + coche.getModelo() + ", Color: " + coche.getColor() + ", Precio: "
+                                + coche.getPrecio() + ", Año: " + coche.getAnio() + ", Vendido: Sí");
+            } else if (coche.getVendido() == false) {
+                System.out.println(
+                        "ID: " + coche.getId() + ", Matrícula: " + coche.getMatricula() + ", Marca: " + coche.getMarca()
+                                + ", Modelo: " + coche.getModelo() + ", Color: " + coche.getColor() + ", Precio: "
+                                + coche.getPrecio() + ", Año: " + coche.getAnio() + ", Vendido: No");
+            }
+        }
+    }
+
+    // Buscar coches según características
+    // Buscar por matrícula
+    public static void buscarPorMatricula(String matriculaBuscada) {
+        boolean encontrado = false;
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getMatricula().equalsIgnoreCase(matriculaBuscada)) {
+                Coche coche = coches[i];
+                System.out.println("ID: " + coche.getId() + "| Matrícula: " + coche.getMatricula() + "| Marca: "
+                        + coche.getMarca() +
+                        "| Modelo: " + coche.getModelo() + "| Color: " + coche.getColor() + "| Precio: "
+                        + coche.getPrecio() + "| Año: " + coche.getAnio());
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("No se encontró ningún coche con la matrícula: " + matriculaBuscada);
+        }
+    }
+
+    // Buscar coche por marca
+    public static void buscarPorMarca(String marcaBuscada) {
+        boolean encontrado = false;
+
+        // Recorremos el arreglo de coches y comparamos la marca de cada coche
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getMarca().equalsIgnoreCase(marcaBuscada)) {
+                // Si la marca coincide mostramos la lista de coches
+                Coche coche = coches[i];
+                System.out.println("ID: " + coche.getId() + "| Matrícula: " + coche.getMatricula() + "| Marca: "
+                        + coche.getMarca() +
+                        "| Modelo: " + coche.getModelo() + "| Color: " + coche.getColor() + "| Precio: "
+                        + coche.getPrecio() + "| Año: " + coche.getAnio());
+                encontrado = true;
+            }
+        }
+
+        // Si no se encuentra ningún coche con la marca indicada
+        if (!encontrado) {
+            System.out.println("No se encontraron coches de la marca: " + marcaBuscada);
+        }
+    }
+
+    // Buscar coche por modelo
+    public static void buscarPorModelo(String modeloBuscado) {
+        boolean encontrado = false;
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getModelo().equalsIgnoreCase(modeloBuscado)) {
+                Coche coche = coches[i];
+                System.out.println("ID: " + coche.getId() + "| Matrícula: " + coche.getMatricula() + "| Marca: "
+                        + coche.getMarca() +
+                        "| Modelo: " + coche.getModelo() + "| Color: " + coche.getColor() + "| Precio: "
+                        + coche.getPrecio() + "| Año: " + coche.getAnio());
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("No se encontró ningún coche con el modelo: " + modeloBuscado);
+        }
+    }
+
+    // Buscar coche por color
+    public static void buscarPorColor(String colorBuscado) {
+        boolean encontrado = false;
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getColor().equals(colorBuscado)) {
+                Coche coche = coches[i];
+                System.out.println("ID: " + coche.getId() + "| Matrícula: " + coche.getMatricula() + "| Marca: "
+                        + coche.getMarca() +
+                        "| Modelo: " + coche.getModelo() + "| Color: " + coche.getColor() + "| Precio: "
+                        + coche.getPrecio() + "| Año: " + coche.getAnio());
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("No se encontró ningún coche con el color: " + colorBuscado);
+        }
+    }
+
+    // Buscar coche por precio (mayor que)
+    public static void buscarPorPrecio(double precioBuscado) {
+        boolean encontrado = false;
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getPrecio() > precioBuscado) {
+                Coche coche = coches[i];
+                System.out.println("ID: " + coche.getId() + "| Matrícula: " + coche.getMatricula() + "| Marca: "
+                        + coche.getMarca() +
+                        "| Modelo: " + coche.getModelo() + "| Color: " + coche.getColor() + "| Precio: "
+                        + coche.getPrecio() + "| Año: " + coche.getAnio());
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("No se encontró ningún coche con un precio mayor a: " + precioBuscado);
+        }
+    }
+
+    // Buscar coche por año
+    public static void buscarPorAnio(int anioBuscado) {
+        boolean encontrado = false;
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getAnio() == anioBuscado) {
+                Coche coche = coches[i];
+                System.out.println("ID: " + coche.getId() + "| Matrícula: " + coche.getMatricula() + "| Marca: "
+                        + coche.getMarca() +
+                        "| Modelo: " + coche.getModelo() + "| Color: " + coche.getColor() + "| Precio: "
+                        + coche.getPrecio() + "| Año: " + coche.getAnio());
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("No se encontró ningún coche del año: " + anioBuscado);
+        }
+    }
+    
+    // Buscar por id
+    public static void buscarPorId(int idBuscado) {
+        boolean encontrado = false;
+
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getId() == idBuscado) {
+                // Si se encuentra el coche, mostrar los detalles
+                Coche coche = coches[i];
+                System.out.println("Coche encontrado:");
+                System.out.println("ID: " + coche.getId() + "| Matrícula: " + coche.getMatricula() + "| Marca: "
+                        + coche.getMarca() +
+                        "| Modelo: " + coche.getModelo() + "| Color: " + coche.getColor() + "| Precio: "
+                        + coche.getPrecio() + "| Año: " + coche.getAnio());
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("No se encontró un coche con el ID: " + idBuscado);
+
+        }
+    }
+
+    // Método para buscar coches por estado de venta (vendido/no vendido)
+    public static void buscarPorEstadoVenta(boolean vendidoBuscado) {
+        boolean encontrado = false;
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getVendido() == vendidoBuscado) {
+                Coche coche = coches[i];
+                System.out.println("ID: " + coche.getId() + "| Matrícula: " + coche.getMatricula() + "| Marca: "
+                        + coche.getMarca() +
+                        "| Modelo: " + coche.getModelo() + "| Color: " + coche.getColor() + "| Precio: "
+                        + coche.getPrecio() + "| Año: " + coche.getAnio());
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            if (vendidoBuscado) {
+                System.out.println("No se encontró ningún coche vendido.");
+            } else {
+                System.out.println("No se encontró ningún coche no vendido.");
+            }
+        }
+    }
+
+    // Método para eliminar coches
+    public static String eliminarCoche(int idAEliminar) {
+        boolean encontrado = false;
+
+        // Buscamos el coche con el ID dado
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getId() == idAEliminar) {
+                // Desplazamos los coches que están después del coche eliminado
+                for (int j = i; j < numCoches - 1; j++) {
+                    coches[j] = coches[j + 1]; // Mueve cada coche hacia la izquierda
+                }
+                coches[numCoches - 1] = null; // Limpiar la última posición
+                numCoches--; // Reducir el contador de coches
+
+                encontrado = true;
+                return "Coche con ID " + idAEliminar + " eliminado exitosamente.";
+            }
+        }
+
+        // Si no encontramos el coche con el ID dado
+        if (!encontrado) {
+            return "No se encontró un coche con ID " + idAEliminar;
+        }
+        return "";
+    }
+
+    // Método para comprobar si existe algún coche con el ID proporcionado
+    public static boolean existeCoche(int id) {
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getId() == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Método para editar coches
+    public static void editarCoche(int id, String matricula, String marca, String modelo, String color, double precio,
+            int anio) {
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getId() == id) {
+                coches[i].setMatricula(matricula);
+                coches[i].setMarca(marca);
+                coches[i].setModelo(modelo);
+                coches[i].setColor(color);
+                coches[i].setPrecio(precio);
+                coches[i].setAnio(anio);
+            }
+        }
+    }
+
+    // Método para vender coches
+    public static void venderCoche(int id, boolean vender) {
+        for (int i = 0; i < numCoches; i++) {
+            if (coches[i].getId() == id) {
+                coches[i].setVendido(vender);
+            }
+        }
+    }
+
+    // Métodos para obtener los espacios libres y ocupados
+    public static int obtenerEspaciosLibres() {
+        return coches.length - numCoches;
+    }
+
+    public static int obtenerEspaciosOcupados() {
+        return numCoches;
+    }
+
+}
---
+++ src/concesionario/Concesionario.java
@@ -0,0 +1,203 @@
+package concesionario;
+
+import java.util.Scanner;
+
+public class Concesionario {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int opcion;
+		System.out.println(
+				"Este programa permite gestionar un catálogo de vehículos, proporcionando funciones para \nagregar, mostrar, buscar, eliminar, editar y vender coches.");
+		try {
+			do {
+				System.out.println("\n-- Concesionario de Coches --");
+				System.out.println("1. Agregar coche");
+				System.out.println("2. Mostrar lista de coches");
+				System.out.println("3. Buscar coche según características");
+				System.out.println("4. Eliminar coche");
+				System.out.println("5. Editar información de coche");
+				System.out.println("6. Vender coche");
+				System.out.println("7. Espacios restantes");
+				System.out.println("8. Espacios ocupados");
+				System.out.println("9. Salir");
+				System.out.print("Ingrese una opción: ");
+				opcion = Integer.parseInt(scanner.nextLine());
+
+				switch (opcion) {
+					case 1:
+						System.out.print("\nMatrícula: ");
+						String matricula = scanner.nextLine();
+						System.out.print("Marca: ");
+						String marca = scanner.nextLine();
+						System.out.print("Modelo: ");
+						String modelo = scanner.nextLine();
+						System.out.print("Color: ");
+						String color = scanner.nextLine();
+						System.out.print("Precio: ");
+						double precio = Double.parseDouble(scanner.nextLine());
+						System.out.print("Año: ");
+						int anio = Integer.parseInt(scanner.nextLine());
+						boolean vendido = false;
+
+						// Creamos coche con los atributos anteriormente solicitados
+						Coche nuevoCoche = new Coche(matricula, marca, modelo, color, precio, anio, vendido);
+						System.out.println(Coche.agregarCoche(nuevoCoche));
+						break;
+
+					case 2:
+						Coche.mostrarCoches();
+						break;
+					case 3:
+						int opcion2;
+						do {
+							System.out.println("\n-- Buscar coches por característica --");
+							System.out.println("1. ID");
+							System.out.println("2. Matrícula");
+							System.out.println("3. Marca");
+							System.out.println("4. Modelo");
+							System.out.println("5. Color");
+							System.out.println("6. Precio");
+							System.out.println("7. Año");
+							System.out.println("8. Estado de venta (Vendidos o No vendidos)");
+							System.out.println("9. Salir");
+							System.out.print("Ingrese una opción: ");
+							opcion2 = Integer.parseInt(scanner.nextLine());
+
+							switch (opcion2) {
+								case 1:
+									System.out.print("Ingrese matrícula para buscar: ");
+									int idBuscado = Integer.parseInt(scanner.nextLine());
+									Coche.buscarPorId(idBuscado);
+									break;
+								case 2:
+									System.out.print("Ingrese matrícula para buscar: ");
+									String matriculaBuscada = scanner.nextLine();
+									Coche.buscarPorMatricula(matriculaBuscada);
+									break;
+								case 3:
+									System.out.print("Ingrese marca para buscar: ");
+									String marcaBuscada = scanner.nextLine();
+									Coche.buscarPorMarca(marcaBuscada);
+									break;
+								case 4:
+									System.out.print("Ingrese modelo para buscar: ");
+									String modeloBuscado = scanner.nextLine();
+									Coche.buscarPorModelo(modeloBuscado);
+									break;
+								case 5:
+									System.out.print("Ingrese color para buscar: ");
+									String colorBuscado = scanner.nextLine();
+									Coche.buscarPorColor(colorBuscado);
+									break;
+								case 6:
+									System.out.print("Ingrese precio para buscar (mayor que): ");
+									double precioBuscado = Double.parseDouble(scanner.nextLine());
+									Coche.buscarPorPrecio(precioBuscado);
+									break;
+								case 7:
+									System.out.print("Ingrese año para buscar: ");
+									int anioBuscado = Integer.parseInt(scanner.nextLine());
+									Coche.buscarPorAnio(anioBuscado);
+									break;
+								case 8:
+									System.out.println("¿Desea buscar coches vendidos o no vendidos?");
+									System.out.println("1. Vendidos");
+									System.out.println("2. No vendidos");
+									System.out.print("Ingrese una opción: ");
+									int estadoVenta = Integer.parseInt(scanner.nextLine());
+
+									if (estadoVenta == 1) {
+										Coche.buscarPorEstadoVenta(true); // Buscar coches vendidos
+									} else if (estadoVenta == 2) {
+										Coche.buscarPorEstadoVenta(false); // Buscar coches no vendidos
+									} else {
+										System.out.println("Opción inválida.");
+									}
+									break;
+
+								case 9:
+									break;
+
+								default:
+									System.out.println("Por favor, seleccione una opción válida.");
+									break;
+							}
+						} while (opcion2 != 9);
+						break;
+
+					case 4:
+						System.out.print("Ingrese el ID del coche a eliminar: ");
+						int idAEliminar = Integer.parseInt(scanner.nextLine());
+						System.out.println(Coche.eliminarCoche(idAEliminar));
+						break;
+
+					case 5:
+						System.out.print("\nIngrese el ID del coche a editar: ");
+						int idEditar = Integer.parseInt(scanner.nextLine());
+
+						// Verificar existencia antes de solicitar datos
+						if (Coche.existeCoche(idEditar)) {
+							System.out.print("Nueva Matrícula: ");
+							String nuevaMatricula = scanner.nextLine();
+							System.out.print("Nueva Marca: ");
+							String nuevaMarca = scanner.nextLine();
+							System.out.print("Nuevo Modelo: ");
+							String nuevoModelo = scanner.nextLine();
+							System.out.print("Nuevo Color: ");
+							String nuevoColor = scanner.nextLine();
+							System.out.print("Nuevo Precio: ");
+							double nuevoPrecio = Double.parseDouble(scanner.nextLine());
+							System.out.print("Nuevo Año: ");
+							int nuevoAnio = Integer.parseInt(scanner.nextLine());
+
+							Coche.editarCoche(idEditar, nuevaMatricula, nuevaMarca, nuevoModelo,
+									nuevoColor, nuevoPrecio, nuevoAnio);
+
+							System.out.println("\nCoche editado correctamente.");
+						} else {
+							System.out.println("\nID del coche no encontrado");
+						}
+						break;
+
+					case 6:
+						System.out.print("\nIntroduzca el ID del coche a eliminar o 0 para salir: ");
+						int idVender = Integer.parseInt(scanner.nextLine());
+						if (Coche.existeCoche(idVender)) {
+							boolean vender = true;
+							Coche.venderCoche(idVender, vender);
+							System.out.println("\nEl coche con ID " + idVender + " ha sido vendido.");
+						} else if (idVender == 0) {
+							System.out.println("\nOperación cancelada");
+						} else {
+							System.out.println("\nID del coche no encontrado");
+						}
+						break;
+
+					case 7:
+						System.out.println("Espacios libres en el concesionario: " + Coche.obtenerEspaciosLibres());
+						break;
+
+					case 8:
+						System.out.println("Espacios ocupados en el concesionario: " + Coche.obtenerEspaciosOcupados());
+						break;
+
+					case 9:
+						System.out.println("Saliendo del programa...");
+						opcion = 9; // Esto hace que el bucle principal termine
+						break;
+
+					default:
+						System.out.println("\nPor favor, seleccione una opción válida.");
+						break;
+				}
+
+			} while (opcion != 9);
+		} catch (NumberFormatException e) {
+			System.out.println("\nError: Ingrese un dato válido.");
+		}
+
+		scanner.close();
+	}
+
+}
